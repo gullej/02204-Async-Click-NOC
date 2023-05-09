dfdf0d9b0b48e441e92ebab07e839d687a255c33
Wombatfreak <madsptak@gmail.com>
2023-05-09 13:23:31 +0200

    Update straight port and crated tb for it

--- async_noc_io_port_straight.vhd
+++ async_noc_io_port_straight.vhd
@@ -6,45 +6,50 @@ USE IEEE.STD_LOGIC_1164.ALL;
 USE work.defs.ALL;
 
 ENTITY asyncoc_io_port_straight IS
+    GENERIC (
+        LOCATION_X            :  integer;
+        LOCATION_Y            :  integer;
+        ADDR_WIDTH            :  integer
+    );
     PORT (
         -- control
-        reset : IN STD_LOGIC;
-        start : IN STD_LOGIC;
+        reset                  : IN STD_LOGIC;
+        start                  : IN STD_LOGIC;
 
         -- from local
-        rx_local_req_in     : IN  STD_LOGIC;
-        rx_local_ack_out    : OUT STD_LOGIC;
-        rx_local_dat_in     : IN  STD_LOGIC_VECTOR(DATA_WIDTH - 1 DOWNTO 0);
+        rx_local_req_in        : IN  STD_LOGIC;
+        rx_local_ack_out       : OUT STD_LOGIC;
+        rx_local_dat_in        : IN  STD_LOGIC_VECTOR(DATA_WIDTH - 1 DOWNTO 0);
 
         -- from internal a
-        rx_internal_a_req_in  : IN STD_LOGIC;
-        rx_internal_a_ack_out : OUT STD_LOGIC;
-        rx_internal_a_dat_in  : IN STD_LOGIC_VECTOR(DATA_WIDTH - 1 DOWNTO 0);
+        rx_internal_a_req_in   : IN STD_LOGIC;
+        rx_internal_a_ack_out  : OUT STD_LOGIC;
+        rx_internal_a_dat_in   : IN STD_LOGIC_VECTOR(DATA_WIDTH - 1 DOWNTO 0);
 
         -- from internal b
-        rx_internal_b_req_in  : IN STD_LOGIC;
-        rx_internal_b_ack_out : OUT STD_LOGIC;
-        rx_internal_b_dat_in  : IN STD_LOGIC_VECTOR(DATA_WIDTH - 1 DOWNTO 0);
+        rx_internal_b_req_in   : IN STD_LOGIC;
+        rx_internal_b_ack_out  : OUT STD_LOGIC;
+        rx_internal_b_dat_in   : IN STD_LOGIC_VECTOR(DATA_WIDTH - 1 DOWNTO 0);
 
         -- from internal c
-        rx_internal_c_req_in  : IN STD_LOGIC;
-        rx_internal_c_ack_out : OUT STD_LOGIC;
-        rx_internal_c_dat_in  : IN STD_LOGIC_VECTOR(DATA_WIDTH - 1 DOWNTO 0);   
+        rx_internal_c_req_in   : IN STD_LOGIC;
+        rx_internal_c_ack_out  : OUT STD_LOGIC;
+        rx_internal_c_dat_in   : IN STD_LOGIC_VECTOR(DATA_WIDTH - 1 DOWNTO 0);   
         
         -- from external
-        rx_external_req_in  : IN  STD_LOGIC;
-        rx_external_ack_out : OUT STD_LOGIC;
-        rx_external_dat_in  : IN  STD_LOGIC_VECTOR(DATA_WIDTH - 1 DOWNTO 0);
+        rx_external_req_in     : IN  STD_LOGIC;
+        rx_external_ack_out    : OUT STD_LOGIC;
+        rx_external_dat_in     : IN  STD_LOGIC_VECTOR(DATA_WIDTH - 1 DOWNTO 0);
 
         -- to external
-        tx_external_req_in  : OUT STD_LOGIC;
-        tx_external_ack_out : IN  STD_LOGIC;
-        tx_external_dat_in  : OUT STD_LOGIC_VECTOR(DATA_WIDTH - 1 DOWNTO 0);
+        tx_external_req_in     : OUT STD_LOGIC;
+        tx_external_ack_out    : IN  STD_LOGIC;
+        tx_external_dat_in     : OUT STD_LOGIC_VECTOR(DATA_WIDTH - 1 DOWNTO 0);
 
         -- to local
-        tx_local_req_in  : OUT STD_LOGIC;
-        tx_local_ack_out : IN  STD_LOGIC;
-        tx_local_dat_in  : OUT STD_LOGIC_VECTOR(DATA_WIDTH - 1 DOWNTO 0);
+        tx_local_req_in       : OUT STD_LOGIC;
+        tx_local_ack_out      : IN  STD_LOGIC;
+        tx_local_dat_in       : OUT STD_LOGIC_VECTOR(DATA_WIDTH - 1 DOWNTO 0);
 
         -- to internal 1
         tx_internal_0_req_in  : OUT STD_LOGIC;
@@ -55,6 +60,9 @@ END asyncoc_io_port_straight;
 
 ARCHITECTURE asyncoc_io_port_straight_arc OF asyncoc_io_port_straight IS
 
+    CONSTANT ROUTER_LOCATION_X  :  STD_LOGIC_VECTOR(ADDR_WIDTH-1 DOWNTO 0) := STD_LOGIC_VECTOR(to_unsigned(LOCATION_X,ADDR_WIDTH));
+    CONSTANT ROUTER_LOCATION_Y  :  STD_LOGIC_VECTOR(ADDR_WIDTH-1 DOWNTO 0) := STD_LOGIC_VECTOR(to_unsigned(LOCATION_y,ADDR_WIDTH)); 
+
     SIGNAL arbiter_a_req_in : STD_LOGIC;
     SIGNAL arbiter_a_ack_out : STD_LOGIC;
     SIGNAL arbiter_a_dat_in : STD_LOGIC_VECTOR(DATA_WIDTH - 1 DOWNTO 0);
@@ -62,8 +70,53 @@ ARCHITECTURE asyncoc_io_port_straight_arc OF asyncoc_io_port_straight IS
     SIGNAL arbiter_b_ack_out : STD_LOGIC;
     SIGNAL arbiter_b_dat_in : STD_LOGIC_VECTOR(DATA_WIDTH - 1 DOWNTO 0);
 
+    SIGNAL fork_outB_req   :  STD_LOGIC;
+    SIGNAL fork_outB_data  :  STD_LOGIC_VECTOR(DATA_WIDTH-1 DOWNTO 0);
+    SIGNAL fork_outB_ack   :  STD_LOGIC;
+
+    SIGNAL fork_outC_req   :  STD_LOGIC;
+    SIGNAL fork_outC_data  :  STD_LOGIC_VECTOR(DATA_WIDTH-1 DOWNTO 0);
+    SIGNAL fork_outC_ack   :  STD_LOGIC;
+
+    SIGNAL mux_sel_req   :  STD_LOGIC;
+    SIGNAL mux_sel_data  :  STD_LOGIC_VECTOR(0 DOWNTO 0);
+    SIGNAL mux_sel_ack   :  STD_LOGIC;
+
+    SIGNAL x : STD_LOGIC;
+    SIGNAL y : STD_LOGIC;
+
+    SIGNAL x_dest  :  STD_LOGIC_VECTOR(ADDR_WIDTH-1 DOWNTO 0); 
+    SIGNAL y_dest  :  STD_LOGIC_VECTOR(ADDR_WIDTH-1 DOWNTO 0); 
+
 BEGIN
 
+mux_sel_req  <=  fork_outC_req after XOR_DELAY + OR2_DELAY*ADDR_WIDTH + AND2_DELAY + NOT1_DELAY;
+
+x_dest  <=  fork_outC_data(DATA_WIDTH-1 DOWNTO DATA_WIDTH-ADDR_WIDTH);
+y_dest  <=  fork_outC_data(DATA_WIDTH-ADDR_WIDTH-1 DOWNTO DATA_WIDTH-ADDR_WIDTH*2);
+
+x <= or(x_dest xor ROUTER_LOCATION_X) AFTER XOR_DELAY + OR2_DELAY*ADDR_WIDTH;
+y <= or(y_dest xor ROUTER_LOCATION_Y) AFTER XOR_DELAY + OR2_DELAY*ADDR_WIDTH;
+
+mux_sel_data(0) <= (not x) and (not y)  AFTER AND2_DELAY + NOT1_DELAY;
+
+
+fork_in : entity work.reg_fork
+Port map (
+  rst         =>  reset,
+  --Input chan
+  inA_req     =>  rx_external_req_in ,
+  inA_data    =>  rx_external_dat_in,
+  inA_ack     =>  rx_external_ack_out,
+  --Output cha
+  outB_req    =>  fork_outB_req ,
+  outB_data   =>  fork_outB_data,
+  outB_ack    =>  fork_outB_ack ,
+  --Output cha
+  outC_req    =>  fork_outC_req ,
+  outC_data   =>  fork_outC_data,
+  outC_ack    =>  fork_outC_ack 
+  );
 
 arbiter_a : entity work.arbiter
 PORT MAP(
@@ -121,13 +174,13 @@ PORT MAP(
     rst => reset,
     
     -- Input port
-    inA_req  =>  rx_external_req_in,    
-    inA_data =>  rx_external_dat_in,    
-    inA_ack  =>  rx_external_ack_out,    
+    inA_req  =>  fork_outB_req , 
+    inA_data =>  fork_outB_data, 
+    inA_ack  =>  fork_outB_ack ,  
     -- Select port
-    inSel_req   => open,  
-    inSel_ack   => open,  
-    selector    => open,  
+    inSel_req   => arbiter_a_req_in,
+    inSel_ack   => arbiter_a_ack_out,
+    selector    => arbiter_a_dat_in(0)
     -- Output chan
     outB_req  =>  tx_local_req_in,   
     outB_data =>  tx_local_dat_in,   
---
+++ sim/async_noc_io_port_straight_tb.vhd
@@ -0,0 +1,161 @@
+--
+-- Test bench for DEMUX_four circuit
+--
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE work.defs.ALL;
+
+ENTITY asyncoc_io_port_straight IS
+END asyncoc_io_port_straight;
+
+ARCHITECTURE STRUCTURE OF asyncoc_io_port_straight IS
+
+    SIGNAL rst : STD_LOGIC;
+    SIGNAL inA_req_TB, inA_ack_TB : STD_LOGIC;
+    SIGNAL inB_req_TB, inB_ack_TB : STD_LOGIC;
+    SIGNAL inC_req_TB, inC_ack_TB : STD_LOGIC;
+    SIGNAL inD_req_TB, inD_ack_TB : STD_LOGIC;
+    SIGNAL inE_req_TB, inE_ack_TB : STD_LOGIC;
+    SIGNAL data_in_TB : STD_LOGIC_VECTOR(15 DOWNTO 0);
+    SIGNAL data_b_TB, data_c_TB, data_d_TB, data_e_TB : STD_LOGIC_VECTOR(15 DOWNTO 0);
+
+    SIGNAL arbiter_inA_req_TB    :  STD_LOGIC;
+    SIGNAL arbiter_inA_ack_TB    :  STD_LOGIC;
+    SIGNAL arbiter_inA_data_TB   :  STD_LOGIC_VECTOR(15 DOWNTO 0);
+    SIGNAL arbiter_inB_req_TB    :  STD_LOGIC;
+    SIGNAL arbiter_inB_ack_TB    :  STD_LOGIC;
+    SIGNAL arbiter_inB_data_TB   :  STD_LOGIC_VECTOR(15 DOWNTO 0);
+    SIGNAL arbiter_outA_req_TB   :  STD_LOGIC;
+    SIGNAL arbiter_outA_ack_TB   :  STD_LOGIC;
+    SIGNAL arbiter_outA_data_TB  :  STD_LOGIC_VECTOR(15 DOWNTO 0);
+    SIGNAL arbiter_inC_req_TB    :  STD_LOGIC;
+    SIGNAL arbiter_inC_ack_TB    :  STD_LOGIC;
+    SIGNAL arbiter_inC_data_TB   :  STD_LOGIC_VECTOR(15 DOWNTO 0);
+    SIGNAL arbiter_inD_req_TB    :  STD_LOGIC;
+    SIGNAL arbiter_inD_ack_TB    :  STD_LOGIC;
+    SIGNAL arbiter_inD_data_TB   :  STD_LOGIC_VECTOR(15 DOWNTO 0);
+
+    
+
+    ATTRIBUTE dont_touch : STRING;
+    ATTRIBUTE dont_touch OF inA_req_TB, inA_ack_TB : SIGNAL IS "true";
+    ATTRIBUTE dont_touch OF inB_req_TB, inB_ack_TB : SIGNAL IS "true";
+    ATTRIBUTE dont_touch OF inC_req_TB, inC_ack_TB : SIGNAL IS "true";
+    ATTRIBUTE dont_touch OF inD_req_TB, inD_ack_TB : SIGNAL IS "true";
+    ATTRIBUTE dont_touch OF inE_req_TB, inE_ack_TB : SIGNAL IS "true";
+
+    ATTRIBUTE dont_touch of arbiter_inA_req_TB    :  SIGNAL IS "true";
+    ATTRIBUTE dont_touch of arbiter_inA_ack_TB    :  SIGNAL IS "true";
+    ATTRIBUTE dont_touch of arbiter_inA_data_TB   :  SIGNAL IS "true";
+    ATTRIBUTE dont_touch of arbiter_inB_req_TB    :  SIGNAL IS "true";
+    ATTRIBUTE dont_touch of arbiter_inB_ack_TB    :  SIGNAL IS "true";
+    ATTRIBUTE dont_touch of arbiter_inB_data_TB   :  SIGNAL IS "true";
+    ATTRIBUTE dont_touch of arbiter_outA_req_TB   :  SIGNAL IS "true";
+    ATTRIBUTE dont_touch of arbiter_outA_ack_TB   :  SIGNAL IS "true";
+    ATTRIBUTE dont_touch of arbiter_outA_data_TB  :  SIGNAL IS "true";
+    ATTRIBUTE dont_touch of arbiter_inC_req_TB    :  SIGNAL IS "true";
+    ATTRIBUTE dont_touch of arbiter_inC_ack_TB    :  SIGNAL IS "true";
+    ATTRIBUTE dont_touch of arbiter_inC_data_TB   :  SIGNAL IS "true";
+    ATTRIBUTE dont_touch of arbiter_inD_req_TB    :  SIGNAL IS "true";
+    ATTRIBUTE dont_touch of arbiter_inD_ack_TB    :  SIGNAL IS "true";
+    ATTRIBUTE dont_touch of arbiter_inD_data_TB   :  SIGNAL IS "true";
+
+BEGIN
+
+    inB_ack_TB <= inB_req_TB AFTER 10 ns;
+    inC_ack_TB <= inC_req_TB AFTER 10 ns;
+    inD_ack_TB <= inD_req_TB AFTER 10 ns;
+    inE_ack_TB <= inE_req_TB AFTER 10 ns;
+
+    stim : PROCESS
+    BEGIN
+        inA_req_TB <= '0';
+        rst <= '1';
+        WAIT FOR 100 ns;
+        rst <= '0';
+        WAIT FOR 100 ns;
+
+        inA_req_TB <= '1';
+        data_in_TB <= "0000" & x"123";
+
+        WAIT UNTIL inE_ack_TB = '1';
+
+        inA_req_TB <= '0';
+        
+        WAIT FOR 50 ns;
+
+        inA_req_TB <= '1';
+        data_in_TB <= "0101" & x"567";
+
+        WAIT UNTIL inD_ack_TB = '1';
+
+        inA_req_TB <= '0';
+
+        WAIT FOR 50 ns;
+
+        inA_req_TB <= '1';
+        data_in_TB <= "0001" & x"9AB";
+
+        WAIT UNTIL inC_ack_TB = '1';
+
+        inA_req_TB <= '0';
+
+        WAIT FOR 50 ns;
+
+        inA_req_TB <= '1';
+        data_in_TB <= "0100" & x"DEF";
+
+        WAIT UNTIL inB_ack_TB = '1';
+
+        inA_req_TB <= '0';
+
+        WAIT FOR 50 ns;
+        ASSERT 0 = 1 REPORT "Bye" SEVERITY failure;
+    END PROCESS;
+
+    diagonal_DUT : entity work.asyncoc_io_port_straight
+        GENERIC (
+            LOCATION_X             =>  1,
+            LOCATION_Y             =>  1,
+            ADDR_WIDTH             =>  2,
+        );
+        PORT (
+            -- control
+            reset                  =>  rst,
+            start                  =>  '0',
+            -- from local
+            rx_local_req_in        =>  arbiter_inA_req_TB ,
+            rx_local_ack_out       =>  arbiter_inA_ack_TB ,
+            rx_local_dat_in        =>  arbiter_inA_data_TB,
+            -- from internal a
+            rx_internal_a_req_in   =>  rbiter_inB_req_TB ,
+            rx_internal_a_ack_out  =>  rbiter_inB_ack_TB ,
+            rx_internal_a_dat_in   =>  rbiter_inB_data_TB,
+            -- from internal b
+            rx_internal_b_req_in   =>  arbiter_inC_req_TB ,
+            rx_internal_b_ack_out  =>  arbiter_inC_ack_TB ,
+            rx_internal_b_dat_in   =>  arbiter_inC_data_TB,
+            -- from internal c
+            rx_internal_c_req_in   =>  arbiter_inD_req_TB ,
+            rx_internal_c_ack_out  =>  arbiter_inD_ack_TB ,
+            rx_internal_c_dat_in   =>  arbiter_inD_data_TB,
+
+            -- from external
+            rx_external_req_in     =>  inA_req_TB,
+            rx_external_ack_out    =>  inA_ack_TB,
+            rx_external_dat_in     =>  data_in_TB,
+            -- to external
+            tx_external_req_in     =>  inC_req_TB,
+            tx_external_ack_out    =>  inC_ack_TB,
+            tx_external_dat_in     =>  data_c_TB,
+            -- to local
+            tx_local_req_in        =>  inD_req_TB,
+            tx_local_ack_out       =>  inD_ack_TB,
+            tx_local_dat_in        =>  data_d_TB,
+            -- to internal 1
+            tx_internal_0_req_in   =>  inE_req_TB,
+            tx_internal_0_ack_out  =>  inE_ack_TB,
+            tx_internal_0_dat_in   =>  data_e_TB
+        );
+
+    END STRUCTURE;
